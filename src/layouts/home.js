7e614d8f9668bf7571eefba2d0954ed12e49b15a
Bluette1 <marylene.sawyer@gmail.com>
2020-10-14 18:02:09 +0200

    Add `create project btn` and `create todo btn`

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -7,7 +7,7 @@ class home {
     homeDivWrapper.appendChild(listH1);
     projects.forEach((project) => {
       const projectDiv = document.createElement('div');
-      projectDiv.setAttribute('class', 'project-container d-flex justify-content-between align-items-center');
+      projectDiv.setAttribute('class', 'project-container pl-4 pr-4  d-flex justify-content-between align-items-center');
 
       const title = document.createElement('h4');
       title.textContent = project.title;
@@ -18,7 +18,21 @@ class home {
       projectDiv.appendChild(toDos);
 
       homeDivWrapper.appendChild(projectDiv);
+
     });
+
+    const btnsDiv = document.createElement('div');
+    btnsDiv.setAttribute('class', 'd-flex justify-content-between align-items-center');
+    const createProjectBtn = document.createElement('button');
+    createProjectBtn.setAttribute('class', 'mt-5 btn-primary create-project-btn p-4')
+    createProjectBtn.textContent = 'Create a project';
+    btnsDiv.appendChild(createProjectBtn);
+
+    const createToDoBtn = document.createElement('button');
+    createToDoBtn.setAttribute('class', 'ml-5 mt-5 btn-primary create-project-btn p-4')
+    createToDoBtn.textContent = 'Create a todo';
+    btnsDiv.appendChild(createToDoBtn);
+    homeDivWrapper.appendChild(btnsDiv);
     rootElement.appendChild(homeDivWrapper);
   }
 }
