ab2ca0f067dfac0883e0ed9650a0491e5276dca1
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-14 13:18:40 +0200

    Add styling for the form

--- src/layouts/projects-ui.js
+++ src/layouts/projects-ui.js
@@ -3,6 +3,7 @@
 class ProjectUI {
     static displayCreateProjectForm = (element) => {
       const formDivWrapper = document.createElement('div');
+      formDivWrapper.setAttribute('class', 'width-40 centered-horizontal padding-y-5');
       const formTitle = document.createElement('title');
       const formH1 = document.createElement('h1');
       formH1.textContent = 'Create a new project';
@@ -11,11 +12,13 @@ class ProjectUI {
 
       const formElement = document.createElement('form');
       const projectNameDiv = document.createElement('div');
+      projectNameDiv.setAttribute('class', 'form-group');
       const projectNameLabel = document.createElement('label');
       projectNameLabel.textContent = 'Project Name: ';
       const projectNameInputField = document.createElement('input');
       projectNameInputField.setAttribute('type', 'text');
       projectNameInputField.setAttribute('id', 'project-name');
+      projectNameInputField.setAttribute('class', 'form-control form-control-sm');
       projectNameInputField.setAttribute('placeholder', 'Enter the project name here ... ');
       projectNameDiv.appendChild(projectNameLabel);
       projectNameDiv.appendChild(projectNameInputField);
@@ -26,7 +29,7 @@ class ProjectUI {
 
       const formSubmitBtn = document.createElement('button');
       formSubmitBtn.setAttribute('type', 'button');
-      formSubmitBtn.setAttribute('class', 'btn btn-primary');
+      formSubmitBtn.setAttribute('class', 'btn btn-sm btn-primary');
       formSubmitBtn.textContent = 'Save Project';
       formSubmitBtnDiv.appendChild(formSubmitBtn);
 
