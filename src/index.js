d3af7e30e7043c14d5c224663c7e4a47d01a2d81
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-14 15:45:24 +0200

    Add some styling of the navbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './assets/styles/style.scss';
 
 import ProjectUI from './layouts/projects-ui';
 import NavBarUI from './layouts/navbar';
-// import saveBtnListenToClick from './components/app';
 import Project from './components/project';
 
 const content = document.querySelector('#root');
@@ -13,7 +12,7 @@ ProjectUI.displayCreateProjectForm(content);
 
 const projectName = document.querySelector('#project-name');
 const submitBtn = document.querySelector('#submit-project-btn');
-// saveBtnListenToClick({ submitBtn, projectName });
+
 submitBtn.addEventListener('click', () => {
   const projectObj = new Project(projectName.value);
   projectObj.addProject();
--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -1,17 +1,51 @@
 class NavBarUI {
   static displayNavbar = (rootElement) => {
-    const navEl = document.createElement('nav');
-    navEl.setAttribute('class', 'nav-bar navbar');
-    const navItems = document.createElement('ul');
-    navItems.setAttribute('class', 'nav-bar-ul w-100 d-flex justify-content-between justify-content-md-center');
-    const toDoListLink = document.createElement('li');
-    toDoListLink.textContent = 'To Do List';
-    const homeLink = document.createElement('li');
-    homeLink.textContent = 'Home';
-    navItems.appendChild(homeLink);
-    navItems.appendChild(toDoListLink);
-    navEl.appendChild(navItems);
-    rootElement.append(navEl);
+    const headerElt = document.createElement('header');
+    headerElt.setAttribute('class', 'primary-bg centered-h-v width-100');
+    const navElt = document.createElement('nav');
+    navElt.setAttribute('class', 'centered-h-v width-100 flex-md-horizontal-right');
+    const navUl = document.createElement('ul');
+    navUl.setAttribute('class', 'flex-horizontal-center width-100 width-md-60');
+    const navLiHome = document.createElement('li');
+    navLiHome.setAttribute('class', 'padding-1 margin-x-1 active home');
+    navLiHome.setAttribute('id', 'nav-item');
+    const navLiContact = document.createElement('li');
+    navLiContact.setAttribute('class', 'padding-1 margin-x-1 contact');
+    navLiContact.setAttribute('id', 'nav-item');
+    const navLiMenu = document.createElement('li');
+    navLiMenu.setAttribute('class', 'padding-1 margin-x-1 menu');
+    navLiMenu.setAttribute('id', 'nav-item');
+    const homeIcon = document.createElement('i');
+    homeIcon.setAttribute('class', 'fa fa-home hidden-sm');
+    homeIcon.setAttribute('aria-hidden', 'true');
+    const contactIcon = document.createElement('i');
+    contactIcon.setAttribute('class', 'fa fa-address-card hidden-sm');
+    contactIcon.setAttribute('aria-hidden', 'true');
+    const menuIcon = document.createElement('i');
+    menuIcon.setAttribute('class', 'fa fa-bars hidden-sm');
+    menuIcon.setAttribute('aria-hidden', 'true');
+    const homeText = document.createTextNode('Home');
+    const contactText = document.createTextNode('New project');
+    const menuText = document.createTextNode('Menu');
+
+    navLiHome.appendChild(homeIcon);
+    navLiHome.appendChild(homeText);
+
+    navLiContact.appendChild(contactIcon);
+    navLiContact.appendChild(contactText);
+
+    navLiMenu.appendChild(menuIcon);
+    navLiMenu.appendChild(menuText);
+
+    navUl.appendChild(navLiHome);
+    navUl.appendChild(navLiContact);
+    navUl.appendChild(navLiMenu);
+
+    navElt.appendChild(navUl);
+
+    headerElt.appendChild(navElt);
+
+    rootElement.append(headerElt);
   }
 }
 
